a350307b0c833cae015b72233efe283846283566
yukke42 <yusuke.muramatsu@tier4.jp>
2022-09-29 05:41:50 +0900

    feat: length fitting
    Signed-off-by: yukke42 <yusuke.muramatsu@tier4.jp>

--- perception/lidar_centerpoint/src/node.cpp
+++ perception/lidar_centerpoint/src/node.cpp
@@ -27,8 +27,11 @@
 #include <pcl/filters/voxel_grid.h>
 
 #ifdef ROS_DISTRO_GALACTIC
+#include <tf2_eigen/tf2_eigen.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #else
+#include <tf2_eigen/tf2_eigen.hpp>
+
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #endif
 
@@ -160,7 +163,7 @@ void LidarCenterPointNode::pointCloudCallback(
   output_msg.header = input_pointcloud_msg->header;
   output_msg.objects = iou_bev_nms_.apply(raw_objects);
 
-  const double correction_length_min_threshold = 5.0;
+  const double correction_min_length = 5.0;
   const auto voxelized_pcl_ptr = detector_ptr_->getVoxelizedPointCloudPtr();
   pcl::PointCloud<pcl::PointXYZ>::Ptr downsampled_pcl_ptr(new pcl::PointCloud<pcl::PointXYZ>);
   downsampled_pcl_ptr->reserve(voxelized_pcl_ptr->size());
@@ -169,44 +172,90 @@ void LidarCenterPointNode::pointCloudCallback(
   vg_filter.setLeafSize(0.32, 0.32, 4.0);
   vg_filter.filter(*downsampled_pcl_ptr);
 
+  autoware_auto_perception_msgs::msg::DetectedObjects debug_raw_shape_objects_msg;
+  debug_raw_shape_objects_msg.header = input_pointcloud_msg->header;
   pcl::PointCloud<pcl::PointXYZ>::Ptr debug_object_pc_pcl(new pcl::PointCloud<pcl::PointXYZ>);
   debug_object_pc_pcl->reserve(voxelized_pcl_ptr->size());
   auto valid_point = [](double p, double width) -> bool { return (p >= -width) && (p <= width); };
-  for (const auto & object : output_msg.objects) {
+  auto get_sqr_dist = [](double a, double b) -> double { return a * a + b * b; };
+  for (auto & object : output_msg.objects) {
     const auto label = perception_utils::getHighestProbLabel(object.classification);
     if (!perception_utils::isLargeVehicle(label)) {
       continue;
     }
 
-    if (object.shape.dimensions.x < correction_length_min_threshold) {
+    auto & dimensions = object.shape.dimensions;
+    if (dimensions.x < correction_min_length) {
       continue;
     }
 
-    const auto & pose = perception_utils::getPose(object);
-    const auto & dimensions = object.shape.dimensions;
-    // const auto obj_2d_polygon = tier4_autoware_utils::toPolygon2d(pose, object.shape);
-    const auto obj_position = Eigen::Vector3d(pose.position.x, pose.position.y, pose.position.z);
-    const auto obj_orientation = Eigen::Quaterniond(
-      pose.orientation.w, pose.orientation.x, pose.orientation.y, pose.orientation.z);
+    auto & pose = object.kinematics.pose_with_covariance.pose;
+    Eigen::Affine3d obj2base_transform;
+    tf2::fromMsg(pose, obj2base_transform);
+
+    double min_x_obj_coord(std::numeric_limits<double>::max()),
+      max_x_obj_coord(std::numeric_limits<double>::min());
     for (const auto & pc : *downsampled_pcl_ptr) {
-      const double sqrt_dist_between_point_and_centroid =
-        std::pow(pc.x - pose.position.x, 2) + std::pow(pc.y - pose.position.y, 2);
-      const double sqrt_length_of_half_diagonal =
-        std::pow(dimensions.x, 2) + std::pow(dimensions.y, 2);
-      if (sqrt_dist_between_point_and_centroid > sqrt_length_of_half_diagonal) {
+      const double sqr_dist_between_point_and_centroid =
+        get_sqr_dist(pc.x - pose.position.x, pc.y - pose.position.y);
+      const double sqrt_length_of_half_diagonal = get_sqr_dist(dimensions.x, dimensions.y);
+      if (sqr_dist_between_point_and_centroid > sqrt_length_of_half_diagonal) {
         continue;
       }
 
-      tier4_autoware_utils::Point3d p_base_coord(pc.x, pc.y, pc.z);
-      const auto p_obj_coord = obj_orientation.inverse() * (p_base_coord - obj_position);
-      const bool within_length = valid_point(p_obj_coord.x(), dimensions.x * 0.5f);
-      const bool within_width = valid_point(p_obj_coord.y(), dimensions.y * 0.5f);
-      const bool within_height = valid_point(p_obj_coord.z(), dimensions.z * 0.5f);
-      if (!(within_length && within_width && within_height)) {
+      Eigen::Vector3d p_base_coord(pc.x, pc.y, pc.z);
+      const auto p_obj_coord = obj2base_transform.inverse() * p_base_coord;
+      if (!valid_point(p_obj_coord.x(), dimensions.x * 0.5f)) {
         continue;
+      }
+      if (!valid_point(p_obj_coord.y(), dimensions.y * 0.5f)) {
+        continue;
+      }
+      if (!valid_point(p_obj_coord.z(), dimensions.z * 0.5f)) {
+        continue;
+      }
+
+      if (p_obj_coord.x() < min_x_obj_coord) {
+        min_x_obj_coord = p_obj_coord.x();
+      }
+      if (p_obj_coord.x() > max_x_obj_coord) {
+        max_x_obj_coord = p_obj_coord.x();
       }
 
       debug_object_pc_pcl->emplace_back(pcl::PointXYZ(pc.x, pc.y, pc.z));
+    }
+
+    const double new_length = max_x_obj_coord - min_x_obj_coord;
+    if (new_length <= correction_min_length) {
+      const auto & width = dimensions.y;
+      debug_raw_shape_objects_msg.objects.emplace_back(object);
+
+      std::vector<Eigen::Vector3d> corners;
+      corners.push_back(
+        obj2base_transform * Eigen::Vector3d(dimensions.x * 0.5, dimensions.y * 0.5, 0.0));
+      corners.push_back(
+        obj2base_transform * Eigen::Vector3d(-dimensions.x * 0.5, dimensions.y * 0.5, 0.0));
+      corners.push_back(
+        obj2base_transform * Eigen::Vector3d(dimensions.x * 0.5, -dimensions.y * 0.5, 0.0));
+      corners.push_back(
+        obj2base_transform * Eigen::Vector3d(-dimensions.x * 0.5, -dimensions.y * 0.5, 0.0));
+
+      const auto nearest_corner =
+        *(std::min_element(corners.begin(), corners.end(), [](const auto & a, const auto & b) {
+          return a.norm() < b.norm();
+        }));
+
+      Eigen::Vector3d nearest_corner_obj_coord = obj2base_transform.inverse() * nearest_corner;
+      Eigen::Vector3d ex = Eigen::Vector3d(nearest_corner_obj_coord.x(), 0, 0).normalized();
+      Eigen::Vector3d ey = Eigen::Vector3d(0, nearest_corner_obj_coord.y(), 0).normalized();
+      Eigen::Vector3d new_position =
+        nearest_corner -
+        obj2base_transform.rotation() * (ex * correction_min_length * 0.5 + ey * width * 0.5);
+
+      dimensions.x = correction_min_length;
+
+      pose.position.x = new_position.x();
+      pose.position.y = new_position.y();
     }
   }
 
@@ -241,6 +290,9 @@ void LidarCenterPointNode::pointCloudCallback(
     debug_object_pc_msg.header = input_pointcloud_msg->header;
     debug_publisher_ptr_->publish<sensor_msgs::msg::PointCloud2>(
       "debug/object_pointcloud", debug_object_pc_msg);
+
+    debug_publisher_ptr_->publish<autoware_auto_perception_msgs::msg::DetectedObjects>(
+      "debug/raw_shape_objects", debug_raw_shape_objects_msg);
   }
 }
 
